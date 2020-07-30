b0915955deb8ef0efd2bfb986478517f61d13b8d
kor-Randy <randy_95@naver.com>
2020-07-31 02:36:52 +0900

    Create HomeVC, ViewModel Add HomeVC in StoryBoard

---
+++ harusal2-iOS/Source/View/Home/HomeViewController.swift
@@ -0,0 +1,44 @@
+//
+//  HomeViewController.swift
+//  harusal2-iOS
+//
+//  Created by 지현우 on 2020/07/31.
+//  Copyright © 2020 nexters. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: BaseViewController {
+    
+    @IBOutlet weak var menuBarButtonItem : UIBarButtonItem!
+    @IBOutlet weak var receiptBarButtonItem : UIBarButtonItem!
+    @IBOutlet weak var todayMoneyDescriptionLabel : UILabel!
+    @IBOutlet weak var todayMoneyLabel : UILabel!
+    @IBOutlet weak var leftDayForMonthLabel : UILabel!
+    @IBOutlet weak var leftMoneyForMonthLabel : UILabel!
+    @IBOutlet weak var addreceiptButton : UIButton!
+    @IBOutlet weak var seeReceiptView : UIButton!
+    @IBOutlet weak var containLeftLabelView : UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func setConstraints() {
+        addreceiptButton.layer.cornerRadius = addreceiptButton.frame.width/2
+        containLeftLabelView.roundView(by: 80)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ harusal2-iOS/Source/View/Home/HomeViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewModel.swift
+//  harusal2-iOS
+//
+//  Created by 지현우 on 2020/07/31.
+//  Copyright © 2020 nexters. All rights reserved.
+//
+
+import Foundation
+
+class HomeViewModel : ViewModelType{
+    
+    struct Inputs {
+        
+    }
+    
+    struct Outputs {
+        
+    }
+    
+}
