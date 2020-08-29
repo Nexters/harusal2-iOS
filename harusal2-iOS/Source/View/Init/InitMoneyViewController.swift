84783aff5331944938702f64e492eb5fcabac035
YChaeeun <ychae.leah@gmail.com>
2020-08-29 14:23:55 +0900

    키보드 위 툴바 - Done 버튼 누르면 다음으로 이이동

--- harusal2-iOS/Source/View/Init/InitMoneyViewController.swift
+++ harusal2-iOS/Source/View/Init/InitMoneyViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class InitMoneyViewController: UIViewController, UITextFieldDelegate {
+class InitMoneyViewController: UIViewController {
     
     @IBOutlet weak var monthBudgetText : UITextField!
     @IBOutlet weak var budgetLabel: UILabel!
@@ -24,9 +24,41 @@ class InitMoneyViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        monthBudgetText.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
         self.monthBudgetText.becomeFirstResponder()
+    }
+}
+
+extension InitMoneyViewController: UITextFieldDelegate {
+    public func textFieldShouldBeginEditing(_ sender: UITextField) -> Bool {
+        setupTextFieldsAccessoryView()
+        return true
+    }
+
+    func setupTextFieldsAccessoryView() {
+        guard monthBudgetText.inputAccessoryView == nil else {
+            return
+        }
+
+        let toolBar: UIToolbar = UIToolbar(frame:CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height:70))
+        toolBar.barStyle = UIBarStyle.black
+        toolBar.isTranslucent = false
+        
+        let flexsibleSpace: UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+
+        let nextButton: UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.done, target: self, action: #selector(didPressNextButton))
+    
+        toolBar.items = [flexsibleSpace, nextButton]
+    
+        monthBudgetText.inputAccessoryView = toolBar
+    }
+
+    @objc func didPressNextButton(button: UIButton) {
+        let storyBoard = UIStoryboard(name: "Sub", bundle:nil)
+        let memberDetailsViewController = storyBoard.instantiateViewController(withIdentifier: "InitDayViewControllerId")
+        self.navigationController?.pushViewController(memberDetailsViewController, animated:true)
     }
 }
